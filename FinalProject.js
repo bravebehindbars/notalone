8cca9dd9425959ce1d344b694d27bda9d3dea90f
bravebehindbars <143886356+bravebehindbars@users.noreply.github.com>
2023-09-02 10:05:49 -0400

    Add files via upload

---
+++ FinalProject.js
@@ -0,0 +1,10 @@
+function Greeting(){
+
+var new_name = prompt("What is your name?");
+
+console.log(new_name);
+
+var message = document.getElementById("message")
+
+alert ("Hi " + new_name  + "!" + " Welcome to a place of help and healing! " + new_name +"," + " You are AMAZING, you are worthy of love and respect, and you deserve so much better! Please reach out to any of the services listed in 'Resources' for more information in your area.");
+}
